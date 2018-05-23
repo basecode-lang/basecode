85b5c9467291bd8825246ffca8ea367e27dc4ad5
Jeff Panici <jeff.panici@panici-software.com>
2018-05-23 17:51:34 -0600

    added license and copyright header

--- parser/ast.cpp
+++ parser/ast.cpp
@@ -1,3 +1,14 @@
+// ----------------------------------------------------------------------------
+//
+// Basecode Bootstrap Compiler
+// Copyright (C) 2018 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE.md file.
+//
+// ----------------------------------------------------------------------------
+
 #include <set>
 #include <fmt/format.h>
 #include "ast.h"
