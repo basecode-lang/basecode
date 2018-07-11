63698f95de4fe0581dff41b16a83cc9d0f7f7b22
Jeff Panici <jeff.panici@panici-software.com>
2018-07-11 13:23:01 -0600

    fixed license file name

--- alpha-core/compiler/elements/binary_operator.h
+++ alpha-core/compiler/elements/binary_operator.h
@@ -5,7 +5,7 @@
 // All rights reserved.
 //
 // This software source file is licensed under the terms of MIT license.
-// For details, please read the LICENSE.md file.
+// For details, please read the LICENSE file.
 //
 // ----------------------------------------------------------------------------
 
