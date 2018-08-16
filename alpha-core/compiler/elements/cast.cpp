01efa587f0d0819f209373321985c9027b16c2ac
Jeff Panici <jeff.panici@panici-software.com>
2018-08-16 17:46:56 -0600

    ref #20: start work on cast

--- alpha-core/compiler/elements/cast.cpp
+++ alpha-core/compiler/elements/cast.cpp
@@ -26,14 +26,45 @@ namespace basecode::compiler {
                              _type(type) {
     }
 
+    //
+    // numeric casts
+    // ------------------------------------------------------------------------
+    // casting between two integers of the same size (s32 -> u32) is a no-op
+    // casting from a larger integer to a smaller integer (u32 -> u8) will truncate via move
+    // casting from smaller integer to larger integer (u8 -> u32) will:
+    //  - zero-extend if the source is unsigned
+    //  - sign-extend if the source is signed
+    // casting from float to an integer will round the float towards zero
+    // casting from an integer to a float will produce the floating point representation of the
+    //    integer, rounded if necessary
+    // casting from f32 to f64 is lossless
+    // casting from f64 to f32 will produce the closest possible value, rounded if necessary
+    // casting bool to and integer type will yield 1 or 0
+    // casting any integer type whose LSB is set will yield true; otherwise, false
+    //
     bool cast::on_emit(compiler::session& session) {
         if (_expression == nullptr)
             return true;
-        auto instruction_block = session.assembler().current_block();
+
+        auto& assembler = session.assembler();
+        auto target_reg = assembler.current_target_register();
+        auto instruction_block = assembler.current_block();
+
+        auto temp_reg = register_for(session, _expression);
+        if (!temp_reg.valid)
+            return false;
+
+        assembler.push_target_register(temp_reg.reg);
+        _expression->emit(session);
+        assembler.pop_target_register();
+
+        // XXX: this is a placeholder, need to check conditions above
+        instruction_block->move_reg_to_reg(*target_reg, temp_reg.reg);
+
         instruction_block->current_entry()->comment(
-            fmt::format("XXX: cast<{}> not yet implemented", _type->symbol()->name()),
+            fmt::format("cast<{}>", _type->symbol()->name()),
             session.emit_context().indent);
-        return _expression->emit(session);
+        return true;
     }
 
     element* cast::expression() {
